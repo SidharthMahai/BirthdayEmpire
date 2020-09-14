ea5ae6abf9dd74f940d5a3e1773ddcc37a470c3b
Sidharth5117 <sidharth2136@gmail.com>
2020-09-14 20:46:00 +0530

    Firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,14 @@
 export const environment = {
-  production: true
+  production: true,
+
+firebase: {
+apiKey: "AIzaSyAVjn5cDvVGVMNJVtE5LPxTf-q84zgWWdg",
+    authDomain: "birthdayempire.firebaseapp.com",
+    databaseURL: "https://birthdayempire.firebaseio.com",
+    projectId: "birthdayempire",
+    storageBucket: "birthdayempire.appspot.com",
+    messagingSenderId: "316575077701",
+    appId: "1:316575077701:web:d11ae1034c561e38605a2c"
+}
+
 };
